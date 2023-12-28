fefbd847a8bdca2d68392f57c5cebd56a0ee063b
KHAYAL GULUSH <gulushovxeyal@gmail.com>
2023-12-28 19:41:13 +0400

    Added layout and static files

--- warehouse/warehouse/settings.py
+++ warehouse/warehouse/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Baku'
 
 USE_I18N = True
 
@@ -118,17 +118,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL ='/media/'
+MEDIA_URL = '/media/'
 
-MEDIA_URL='/uploads/'
+MEDIA_URL = '/uploads/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media', 'uploads')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'uploads')
 
 AUTH_USER_MODEL = 'main.myUser'
 
